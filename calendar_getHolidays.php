130bdaa46b0813610f56289ea14b5443ed284f5d
onodera_montecampo <onodera.shinya@montecampo.co.jp>
2021-05-06 16:15:29 +0900

    add google api etc

--- calendar_getHolidays.php
+++ calendar_getHolidays.php
@@ -9,8 +9,11 @@ function getHolidays($year){
     $api_key = 'AIzaSyAt5KTiJE733ESlNqTYXSMs5FDfBZEU0nI';//
     $holidays = array();//祝日をいれる配列
     $holidays_id = 'japanese__ja@holiday.calendar.google.com';
-    #url = sprintf(
-        
-    )
+    $url = sprintf(
+        'https://www.googleapis.com/calendar/v3/calendars/%s/events?'.
+		'key=%s&timeMin=%s&timeMax=%s&maxResults=%d&orderBy=startTime&singleEvents=true',
+        $holidays_id,
+        $api_key,
+    );
 }
 ?>
